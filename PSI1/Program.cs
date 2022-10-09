e562a13015d4536afbf34ad104e805123bd0db7a
Omega <79137917+OmegaLeWolf@users.noreply.github.com>
2022-10-09 19:06:57 +0100

    Finishing touches to the menu // Organization

--- PSI1/Program.cs
+++ PSI1/Program.cs
@@ -14,6 +14,13 @@ namespace PSI1
     {
         static void Main(string[] args)
         {
+            while(true)
+                Menu();
+            
+        }
+
+        static void Menu()
+        {
             //Introdução ao projeto (menu)
 
             while (true)
@@ -22,8 +29,10 @@ namespace PSI1
                 Console.WriteLine("Bem vindo ao Mini Projeto 1 de PSI!");
                 Console.WriteLine("+---------------------------------+");
 
+                //Array de strings para ditar as opções do menu
                 string[] menuarr = new string[3] { "Opção 1", "Opção 2", "Opção 3" };
 
+                //Foreach loop para mostrar ao utilizador as opções
                 foreach (string item in menuarr)
                 {
                     Console.WriteLine(item);
@@ -31,16 +40,27 @@ namespace PSI1
 
                 Console.WriteLine("\nIntroduza a opção desejada: ");
 
-
+                //Recebe o valor inserido
                 string menu_val = Console.ReadLine();
-                Int32.TryParse(menu_val, out int menu_val_int);
+
+                //Verificar se menu_val conversível a int32
+                if (Int32.TryParse(menu_val, out int menu_val_int) == false) 
+                {
+                    Console.WriteLine("Opção Inválida! A retornar ao início.."); 
+                    Thread.Sleep(5000); 
+                    Console.Clear(); 
+
+                    break; 
+                }
+
                 //Console.WriteLine(menu_val_int.GetType());
 
-
+                //Criação de um dicionário de funções porque ao utilizar Switch;Case; o código ficava feio :)
                 Dictionary<int, Func<int>> op = new Dictionary<int, Func<int>>
                 {
                 {1, op0},
                 {2, op1},
+                {3, op2 },
                 };
 
                 op[menu_val_int]();
@@ -48,7 +68,22 @@ namespace PSI1
 
                 Console.ReadKey();
             }
-            
+        }
+
+        static Int32 op0()
+        {
+            //Pedindo Valores ao utilizador
+
+            Console.Write("Olá! Qual o seu nome? ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Olá {0}! Bem vindo ao Mini Projeto 1 de PSI!", name);
+
+            Console.WriteLine("Quantos anos tem?");
+            string age = Console.ReadLine();
+
+            return Convert.ToInt32(age); // Conversão string-int32 + retorna valor
         }
 
         static Int32 op1()
@@ -81,20 +116,11 @@ namespace PSI1
             return 0;
         }
 
-        static Int32 op0()
+        static int op2()
         {
-            //Pedindo Valores ao utilizador
-
-            Console.Write("Olá! Qual o seu nome? ");
-            string name = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Olá {0}! Bem vindo ao Mini Projeto 1 de PSI!", name);
-
-            Console.WriteLine("Quantos anos tem?");
-            string age = Console.ReadLine();
-
-            return Convert.ToInt32(age); // Conversão string-int32 + retorna valor
+            return 0;
         }
+
+        
     }
 }
